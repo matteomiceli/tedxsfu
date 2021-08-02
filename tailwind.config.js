f44f1348dbefdd5fe2c0b8c82838303d745cc8f0
Matteo Miceli <omatteomiceli@gmail.com>
2021-08-02 01:42:41 -0700

    tailwind configured (issues with jit compiler)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   // mode:'jit',
-  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  purge: [
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx}'
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
