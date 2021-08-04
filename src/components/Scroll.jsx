61c028ac2ba7f56e26c2d0084f873066a1ad5ca9
Matteo Miceli <omatteomiceli@gmail.com>
2021-08-03 22:05:34 -0700

    horiztonal scroll working and on its own component

---
+++ src/components/Scroll.jsx
@@ -0,0 +1,22 @@
+import * as React from "react"
+
+function Scroll() {
+	return (
+		<div className="outer-scroll-container ">
+			<div className="inner-scroll-container flex flex-nowrap">
+				<div className="scroll-item bg-green-400">1</div>   
+				<div className="scroll-item bg-blue-400">2</div>   
+				<div className="scroll-item bg-green-400">3</div>   
+				<div className="scroll-item bg-blue-400">4</div>   
+				<div className="scroll-item bg-green-400">5</div>   
+				<div className="scroll-item bg-blue-400">6</div>   
+				<div className="scroll-item bg-green-400">7</div>   
+				<div className="scroll-item bg-blue-400">8</div>   
+				<div className="scroll-item bg-green-400">9</div>   
+				<div className="scroll-item bg-blue-400">10</div>   
+			</div>
+		</div>
+	);
+}
+
+export default Scroll;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,20 +1,13 @@
 import * as React from "react"
 
+import Scroll from '../components/Scroll';
+
 
 const IndexPage = () => {
   return (
-    <main className="scroll-container flex flex-nowrap w-screen overflow-scroll0">
-		<div className="scroll-item h-screen bg-green-400">1</div>   
-		<div className="scroll-item h-screen bg-blue-400">2</div>   
-		<div className="scroll-item h-screen bg-green-400">3</div>   
-		<div className="scroll-item h-screen bg-blue-400">4</div>   
-		<div className="scroll-item h-screen bg-green-400">5</div>   
-		<div className="scroll-item h-screen bg-blue-400">6</div>   
-		<div className="scroll-item h-screen bg-green-400">7</div>   
-		<div className="scroll-item h-screen bg-blue-400">8</div>   
-		<div className="scroll-item h-screen bg-green-400">9</div>   
-		<div className="scroll-item h-screen bg-blue-400">10</div>   
-    </main>
+	<div>
+		<Scroll />
+	</div>
   )
 }
 
