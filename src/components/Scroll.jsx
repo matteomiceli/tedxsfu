b3ce529e128521fc05b5dfd5c21e8bfebc740f62
Matteo Miceli <omatteomiceli@gmail.com>
2021-08-04 02:00:52 -0700

    scroll with snap workign - sample images

--- src/components/Scroll.jsx
+++ src/components/Scroll.jsx
@@ -1,19 +1,21 @@
 import * as React from "react"
 
+import ScrollItem from '../components/ScrollItem'
+
 function Scroll() {
 	return (
 		<div className="outer-scroll-container ">
 			<div className="inner-scroll-container flex flex-nowrap">
-				<div className="scroll-item bg-green-400">1</div>   
-				<div className="scroll-item bg-blue-400">2</div>   
-				<div className="scroll-item bg-green-400">3</div>   
-				<div className="scroll-item bg-blue-400">4</div>   
-				<div className="scroll-item bg-green-400">5</div>   
-				<div className="scroll-item bg-blue-400">6</div>   
-				<div className="scroll-item bg-green-400">7</div>   
-				<div className="scroll-item bg-blue-400">8</div>   
-				<div className="scroll-item bg-green-400">9</div>   
-				<div className="scroll-item bg-blue-400">10</div>   
+				<ScrollItem /> 
+				<div className="scroll-item bg-black text-white text-9xl flex justify-center items-center">2</div>   
+				<div className="scroll-item bg-black text-white text-9xl flex justify-center items-center">3</div>   
+				<div className="scroll-item bg-black text-white text-9xl flex justify-center items-center">4</div>   
+				<div className="scroll-item bg-black text-white text-9xl flex justify-center items-center">5</div>   
+				<div className="scroll-item bg-black text-white text-9xl flex justify-center items-center">6</div>   
+				<div className="scroll-item bg-black text-white text-9xl flex justify-center items-center">7</div>   
+				<div className="scroll-item bg-black text-white text-9xl flex justify-center items-center">8</div>   
+				<div className="scroll-item bg-black text-white text-9xl flex justify-center items-center">9</div>   
+				<div className="scroll-item bg-black text-white text-9xl flex justify-center items-center">10</div>   
 			</div>
 		</div>
 	);
---
+++ src/components/ScrollItem.jsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+function ScrollItem() {
+    return (
+        <div className="scroll-item bg-black text-white text-9xl flex justify-center items-center">
+            1
+        </div>  
+    );
+}
+
+export default ScrollItem;
