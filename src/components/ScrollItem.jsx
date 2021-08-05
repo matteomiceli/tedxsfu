d7f80b5a140b404147f7068fcb48174d19a42a38
Matteo Miceli <omatteomiceli@gmail.com>
2021-08-04 18:32:19 -0700

    added pixi react

--- src/components/ScrollItem.jsx
+++ src/components/ScrollItem.jsx
@@ -1,9 +1,17 @@
 import * as React from "react";
+import { Stage, Graphics } from '@inlet/react-pixi';
+import ReactDOM from 'react-dom';
 
 function ScrollItem({ speaker }) {
+    const draw = React.useCallback((g) => {
+        g.drawRect(50, 150, 120, 120);
+    },[]); 
+
     return (
         <div className={`scroll-item scroll-${speaker.img} bg-black text-white text-9xl flex justify-center items-center`}>
-            
+            <Stage width={'100vw'} height={'100vh'} options={{backgroundColor: 0xffff}}>
+                <Graphics draw={draw}/>
+            </Stage>
         </div>  
     );
 }
