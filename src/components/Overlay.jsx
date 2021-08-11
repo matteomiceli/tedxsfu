5b628a5be3201098a1b2fa292a6914aefacfcdee
Matteo Miceli <omatteomiceli@gmail.com>
2021-08-11 03:55:00 -0700

    gaussian blur applied, had to set rgba value instead of solid hex and opacity

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -1,23 +1,18 @@
 import React, { useCallback, useEffect } from "react";
-import loadable from '@loadable/component';
-const BackdropFilter = loadable(() => import('react-backdrop-filter'))
+import loadable from "@loadable/component";
+const BackdropFilter = loadable(() => import("react-backdrop-filter"));
 
 function Overlay() {
   const isBrowser = typeof window !== "undefined";
 
   return (
-      <BackdropFilter 
-      className="glass h-24 w-48 bg-white absolute opacity-10"
-      filter={"blur(10px)"}
-      canvasFallback={true}
-      html2canvasOpts={{
-          allowTaint: true
-      }}
-      onDraw={() => {
-          console.log("Rendered !");
-      }}
-    ></BackdropFilter>
-  ); 
+    <div>
+      <BackdropFilter
+        className="glass h-24 w-48 z-10"
+        filter={"blur(15px)"}
+      ></BackdropFilter>
+    </div>
+  );
 }
 
 export default Overlay;
