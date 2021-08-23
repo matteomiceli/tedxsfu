cff572a8a47787095e7ccd8e76dfe1a3c7c9fb8c
Matteo Miceli <omatteomiceli@gmail.com>
2021-08-23 11:03:54 -0700

    updating speakers.js

--- src/content/speakers.js
+++ src/content/speakers.js
@@ -3,25 +3,25 @@ const speakers = [
     i: 1,
     speaker: "Test Name",
     img: "speaker1",
-    topic: 'Test title',
+    topic: 'Test Title For TedxSFU 1',
   },
   {
     i: 2,
     speaker: "Test 2",
     img: "speaker2",
-    topic: 'Test title',
+    topic: 'Test Title For TedxSFU 2',
   },
   {
     i: 3,
     speaker: "Test 3",
     img: "speaker3",
-    topic: 'Test title',
+    topic: 'Test Title For TedxSFU 3',
   },
   {
     i: 4,
     speaker: "Test 4",
     img: "speaker4",
-    topic: 'Test title',
+    topic: 'Test Title For TedxSFU 4',
   },
 ];
 
