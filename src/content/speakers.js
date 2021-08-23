27c814c0444fbc65763d01b21f04a9a87b72bf82
Matteo Miceli <omatteomiceli@gmail.com>
2021-08-23 12:49:34 -0700

    text on main pages

--- src/content/speakers.js
+++ src/content/speakers.js
@@ -3,25 +3,29 @@ const speakers = [
     i: 1,
     speaker: "Test Name 1",
     img: "speaker1",
-    topic: 'Test Title For TedxSFU 1',
+    title: 'Test Title For TedxSFU 1',
+    bio: 'Indigenous studies and political science student',
   },
   {
     i: 2,
     speaker: "Test Name 2",
     img: "speaker2",
-    topic: 'Test Title For TedxSFU 2',
+    title: 'Test Title For TedxSFU 2',
+    bio: 'Indigenous studies and political science student',
   },
   {
     i: 3,
     speaker: "Test Name 3",
     img: "speaker3",
-    topic: 'Test Title For TedxSFU 3',
+    title: 'Test Title For TedxSFU 3',
+    bio: 'Indigenous studies and political science student',
   },
   {
     i: 4,
     speaker: "Test Name 4",
     img: "speaker4",
-    topic: 'Test Title For TedxSFU 4',
+    title: 'Test Title For TedxSFU 4',
+    bio: 'Indigenous studies and political science student',
   },
 ];
 
