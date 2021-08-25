933ad9c42b493ff004f91833841e664d659e4752
Alvin Leung <alvinleung2009@gmail.com>
2021-08-25 13:13:47 -0700

    Refactor nav bar & div into wrapper

--- src/pages/team.jsx
+++ src/pages/team.jsx
@@ -4,26 +4,25 @@ import Navbar from "../components/Navbar";
 import TeamScroll from "../components/TeamScroll";
 
 const TeamPage = () => {
-    // team focus
-  const [spyTeam, setTeam] = useState(1); 
+  // team focus
+  const [spyTeam, setTeam] = useState(1);
   // page width
   const [width, setWidth] = useState(0);
   // page scroll location
   const [scroll, setScroll] = useState(0);
 
-    return (
-        <div className="bg-black">
-            <Navbar page={3} />
-            <TeamScroll 
-                spyTeam={spyTeam}
-                setTeam={setTeam}
-                scroll={scroll}
-                setScroll={setScroll}
-                width={width}
-                setWidth={setWidth}
-            />
-        </div>
-    );
-}
+  return (
+    <>
+      <TeamScroll
+        spyTeam={spyTeam}
+        setTeam={setTeam}
+        scroll={scroll}
+        setScroll={setScroll}
+        width={width}
+        setWidth={setWidth}
+      />
+    </>
+  );
+};
 
 export default TeamPage;
