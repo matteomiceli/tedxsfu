51819c20a84f8911d069ad1b95ab465b36b8d7ae
Alvin Leung <alvinleung2009@gmail.com>
2021-08-26 17:55:23 -0700

    Improve spacing and add live content

--- src/content/sponsors.js
+++ src/content/sponsors.js
@@ -1,89 +1,143 @@
 //@ts-check
-const sponsors = [
-  {
-    tier: "platinum",
-    list: [
-      {
-        name: "SFU External Relations",
-        logo: "https://source.unsplash.com/random",
-        about:
-          "Since the creation of Canada’s first Executive MBA in 1968, Simon Fraser University’s Beedie School of Business has emerged as a dynamic teaching and learning setting with a reputation for producing global-class research for the knowledge economy.  With over 26,000 alumni in over 70 countries, SFU Beedie offers lifelong engagement opportunities through annual reunions, educational tune-ups and research events.",
-        website: "https://google.com",
-      },
-      {
-        name: "SFU Beedie School of Business",
-        logo: "https://source.unsplash.com/random",
-        about:
-          "Since the creation of Canada’s first Executive MBA in 1968, Simon Fraser University’s Beedie School of Business has emerged as a dynamic teaching and learning setting with a reputation for producing global-class research for the knowledge economy.  With over 26,000 alumni in over 70 countries, SFU Beedie offers lifelong engagement opportunities through annual reunions, educational tune-ups and research events.",
-        website: "https://google.com",
-      },
-    ],
-  },
-  {
-    tier: "in-kind",
-    list: [
-      {
-        name: "Dead Frog Brewery",
-        logo: "https://source.unsplash.com/random",
-        about:
-          "Dead Frog is a locally owned, award-winning craft brewery born and raised in the Fraser Valley with 20+ craft brews on tap, fresh eats, and good times. They feature creative brews and unique eats that make you want to come back for more!",
-        website: "https://google.com",
-        facebook: "https://google.com",
-        instagram: "https://google.com",
-        twitter: "https://google.com",
-      },
-      {
-        name: "Grounds for Coffee",
-        logo: "https://source.unsplash.com/random",
-        about:
-          "Thank you and shout out to @GroundsYVR for providing these #NiceBuns for our event. Grounds for Coffee has been home to the world's best cinnamon bun since 1993, with locations at Alma & 10th and on The Drive.",
-        website: "https://google.com",
-        facebook: "https://google.com",
-        instagram: "https://google.com",
-        twitter: "https://google.com",
-      },
-      {
-        name: "Grounds for Coffee",
-        logo: "https://source.unsplash.com/random",
-        about:
-          "Thank you and shout out to @GroundsYVR for providing these #NiceBuns for our event. Grounds for Coffee has been home to the world's best cinnamon bun since 1993, with locations at Alma & 10th and on The Drive.",
-        website: "https://google.com",
-        facebook: "https://google.com",
-        instagram: "https://google.com",
-        twitter: "https://google.com",
-      },
-      {
-        name: "Grounds for Coffee",
-        logo: "https://source.unsplash.com/random",
-        about:
-          "Thank you and shout out to @GroundsYVR for providing these #NiceBuns for our event. Grounds for Coffee has been home to the world's best cinnamon bun since 1993, with locations at Alma & 10th and on The Drive.",
-        website: "https://google.com",
-        facebook: "https://google.com",
-        instagram: "https://google.com",
-        twitter: "https://google.com",
-      },
-      {
-        name: "Grounds for Coffee",
-        logo: "https://source.unsplash.com/random",
-        about:
-          "Thank you and shout out to @GroundsYVR for providing these #NiceBuns for our event. Grounds for Coffee has been home to the world's best cinnamon bun since 1993, with locations at Alma & 10th and on The Drive.",
-        website: "https://google.com",
-        facebook: "https://google.com",
-        instagram: "https://google.com",
-        twitter: "https://google.com",
-      },
-      {
-        name: "Grounds for Coffee",
-        logo: "https://source.unsplash.com/random",
-        about:
-          "Thank you and shout out to @GroundsYVR for providing these #NiceBuns for our event. Grounds for Coffee has been home to the world's best cinnamon bun since 1993, with locations at Alma & 10th and on The Drive.",
-        website: "https://google.com",
-        facebook: "https://google.com",
-        instagram: "https://google.com",
-        twitter: "https://google.com",
-      },
-    ],
-  },
-];
+const sponsors = {
+  platinum: [
+    {
+      name: "SFU External Relations",
+      logo: "/images/sponsors/sfu-external-relations.png",
+      about:
+        "Since the creation of Canada’s first Executive MBA in 1968, Simon Fraser University’s Beedie School of Business has emerged as a dynamic teaching and learning setting with a reputation for producing global-class research for the knowledge economy.  With over 26,000 alumni in over 70 countries, SFU Beedie offers lifelong engagement opportunities through annual reunions, educational tune-ups and research events.",
+      website: "https://google.com",
+    },
+    {
+      name: "SFU Beedie School of Business",
+      logo: "/images/sponsors/sfu-beedie.png",
+      about:
+        "Since the creation of Canada’s first Executive MBA in 1968, Simon Fraser University’s Beedie School of Business has emerged as a dynamic teaching and learning setting with a reputation for producing global-class research for the knowledge economy.  With over 26,000 alumni in over 70 countries, SFU Beedie offers lifelong engagement opportunities through annual reunions, educational tune-ups and research events.",
+      website: "https://google.com",
+    },
+  ],
+  inkind: [
+    {
+      name: "Dead Frog Brewery",
+      logo: "/images/sponsors/dead-frong-brewery.png",
+      about:
+        "Dead Frog is a locally owned, award-winning craft brewery born and raised in the Fraser Valley with 20+ craft brews on tap, fresh eats, and good times. They feature creative brews and unique eats that make you want to come back for more!",
+      website: "https://google.com",
+      facebook: "https://google.com",
+      instagram: "https://google.com",
+      twitter: "https://google.com",
+    },
+    {
+      name: "Grounds for Coffee",
+      logo: "/images/sponsors/grounds-for-coffee.png",
+      about:
+        "Thank you and shout out to @GroundsYVR for providing these #NiceBuns for our event. Grounds for Coffee has been home to the world's best cinnamon bun since 1993, with locations at Alma & 10th and on The Drive.",
+      website: "https://google.com",
+      facebook: "https://google.com",
+      instagram: "https://google.com",
+      twitter: "https://google.com",
+    },
+    {
+      name: "Catoro Cafe",
+      logo: "/images/sponsors/catoro-cafe.png",
+      about:
+        "Catoro Cafe is an institution in the community to promote animal welfare, mental wellness, and climate control by means of waste recycling. Catoro consistently engages with the community by offering programs that support local artists, people with mental illness, students, and many more.",
+      website: "https://google.com",
+      facebook: "https://google.com",
+      instagram: "https://google.com",
+      twitter: "https://google.com",
+    },
+    {
+      name: "VNYSA",
+      logo: "/images/sponsors/vnysa.png",
+      about:
+        "Use promo code: TEDXSFU to receive your first 2 months subscription at 50% off (regular price $9.99 USD / month).",
+      website: "https://google.com",
+      facebook: "https://google.com",
+      instagram: "https://google.com",
+      twitter: "https://google.com",
+    },
+    {
+      name: "Steve’s Poke Bar",
+      logo: "/images/sponsors/steves-poke-bar.png",
+      about:
+        "Steve's Poké Bar is grateful to their amazing loyal customers who supported them throughout the pandemic and are proud to be sponsoring groups and community events such as this.",
+      website: "https://google.com",
+      facebook: "https://google.com",
+      instagram: "https://google.com",
+      twitter: "https://google.com",
+    },
+    {
+      name: "Modo Yoga Vancouver",
+      logo: "/images/sponsors/modo-yoga-vancouver.png",
+      about:
+        "The Modo Yoga Vancouver community is joined together by a love of yoga, commitment to protecting the earth, and of course a passion for long walks on the beach. They offer accessible classes that are challenging, healthy, and fun!",
+      website: "https://google.com",
+      facebook: "https://google.com",
+      instagram: "https://google.com",
+      twitter: "https://google.com",
+    },
+    {
+      name: "Red Truck Beer Company",
+      logo: "/images/sponsors/red-truck-beer-company.png",
+      about:
+        "Red Truck Beer is built on the values of a simpler time, when you didn’t need a dictionary to order a drink when trucks went to work, not the mall. That’s why they keep their beer simple and brew it honestly and independently, with no preservatives, pasteurization, or shortcuts.",
+      website: "https://google.com",
+      facebook: "https://google.com",
+      instagram: "https://google.com",
+      twitter: "https://google.com",
+    },
+    {
+      name: "Skoah",
+      logo: "/images/sponsors/skoah.jpg",
+      about:
+        "Skoah is a Vancouver-based skin care product company with locations in Canada and the US. Since 2001 skoah has earned recognition in many well-respected publications including Style, The New York Times, Allure, The Global and Mail, Travel and Leisure, Women’s Wear Daily and Vogue.",
+      website: "https://google.com",
+      facebook: "https://google.com",
+      instagram: "https://google.com",
+      twitter: "https://google.com",
+    },
+    {
+      name: "Nelson the Seagull",
+      logo: "/images/sponsors/nelson-the-seagull.png",
+      about:
+        "Skoah is a Vancouver-based skin care product company with locations in Canada and the US. Since 2001 skoah has earned recognition in many well-respected publications including Style, The New York Times, Allure, The Global and Mail, Travel and Leisure, Women’s Wear Daily and Vogue.",
+      website: "https://google.com",
+      facebook: "https://google.com",
+      instagram: "https://google.com",
+      twitter: "https://google.com",
+    },
+    {
+      name: "Everything Wine",
+      logo: "/images/sponsors/everything-wine.png",
+      about:
+        "Skoah is a Vancouver-based skin care product company with locations in Canada and the US. Since 2001 skoah has earned recognition in many well-respected publications including Style, The New York Times, Allure, The Global and Mail, Travel and Leisure, Women’s Wear Daily and Vogue.",
+      website: "https://google.com",
+      facebook: "https://google.com",
+      instagram: "https://google.com",
+      twitter: "https://google.com",
+    },
+    {
+      name: "Steamworks Brewery",
+      logo: "/images/sponsors/steamworks.png",
+      about:
+        "Skoah is a Vancouver-based skin care product company with locations in Canada and the US. Since 2001 skoah has earned recognition in many well-respected publications including Style, The New York Times, Allure, The Global and Mail, Travel and Leisure, Women’s Wear Daily and Vogue.",
+      website: "https://google.com",
+      facebook: "https://google.com",
+      instagram: "https://google.com",
+      twitter: "https://google.com",
+    },
+    {
+      name: "Old Spaghetti Factory",
+      logo: "/images/sponsors/spaghetti-factory.svg",
+      about:
+        "Skoah is a Vancouver-based skin care product company with locations in Canada and the US. Since 2001 skoah has earned recognition in many well-respected publications including Style, The New York Times, Allure, The Global and Mail, Travel and Leisure, Women’s Wear Daily and Vogue.",
+      website: "https://google.com",
+      facebook: "https://google.com",
+      instagram: "https://google.com",
+      twitter: "https://google.com",
+    },
+  ],
+};
 
 export default sponsors;
--- src/pages/sponsors.jsx
+++ src/pages/sponsors.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import HorizontalScrollContainer from "../components/HorizontalScrollContainer";
 import sponsors from "../content/sponsors";
+import Button from "../components/Button";
+import SocialButton from "../components/SocialButton";
 
 //@ts-check
 const SponsorshipPage = () => {
@@ -9,16 +11,8 @@ const SponsorshipPage = () => {
       <HorizontalScrollContainer>
         <div className="flex flex-row flex-nowrap ml-12">
           <SponsorshipHeader />
-          {sponsors.map((item, index) => {
-            return (
-              <SponsorshipTierSection
-                key={index}
-                tier={item.tier}
-                index={index}
-                sponsorList={item.list}
-              />
-            );
-          })}
+          <PlatinumTierSection sponsorList={sponsors.platinum} />
+          <InKindTierSection sponsorList={sponsors.inkind} />
         </div>
       </HorizontalScrollContainer>
     </div>
@@ -35,11 +29,11 @@ const SponsorshipHeader = () => (
   </div>
 );
 
-const SponsorshipTierSection = ({ index, tier, sponsorList }) => (
-  <div className="mr-8">
+const PlatinumTierSection = ({ sponsorList }) => (
+  <div className="pr-24">
     <div className="flex">
       <div className="inline-block whitespace-nowrap uppercase tracking-widest mr-6 align-middle">
-        {`0${index + 1}`}&nbsp;{tier} Sponsors
+        01&nbsp;Platinum Sponsors
       </div>
       <div className="ruler" />
     </div>
@@ -51,19 +45,66 @@ const SponsorshipTierSection = ({ index, tier, sponsorList }) => (
   </div>
 );
 
+const InKindTierSection = ({ sponsorList }) => (
+  <div className="pr-12">
+    <div className="flex">
+      <div className="inline-block whitespace-nowrap uppercase tracking-widest mr-6 align-middle">
+        02&nbsp;In-kind Sponsors
+      </div>
+      <div className="ruler" />
+    </div>
+    <div className="grid grid-rows-2 grid-flow-col">
+      {sponsorList.map((sponsorInfo, index) => (
+        <SponsorSmall key={index} {...sponsorInfo} />
+      ))}
+    </div>
+  </div>
+);
+
 const Sponsor = ({ name, logo, about, website, facebook, twitter }) => (
   <div className="flex flex-nowrap mt-8">
     <div className="w-sponsor-logo col-start-1 row-start-1 row-end-4 mr-8">
       <img src={logo} alt={`${name}'s logo`} />
     </div>
-
     <div className="w-sponsor-text flex flex-col">
       <h3 className="text-3xl mb-4">{name}</h3>
-      <div className="text-base">{about}</div>
+      <div className="text-base mb-4">{about}</div>
       <div className="">
-        <a href={website} target="_blank">
+        <Button secondary blank href={website}>
           Visit Site
-        </a>
+        </Button>
+      </div>
+    </div>
+  </div>
+);
+
+const SponsorSmall = ({ name, logo, about, website, facebook, twitter }) => (
+  <div className="flex flex-nowrap mt-8 mr-24">
+    <div className="w-sponsor-logo col-start-1 row-start-1 row-end-4 mr-8">
+      <img src={logo} alt={`${name}'s logo`} />
+    </div>
+    <div className="w-sponsor-text flex flex-col" style={{ maxWidth: 333 }}>
+      <h3 className="text-3xl mb-4">{name}</h3>
+      <div className="text-xs mb-4">{about}</div>
+      <div className="flex items-center">
+        <Button secondary blank href={website}>
+          Visit Site
+        </Button>
+        <SocialButton
+          className="ml-4"
+          href="https://google.com"
+          type="facebook"
+        />
+        <SocialButton
+          className="ml-4"
+          href="https://google.com"
+          type="twitter"
+        />
+        <SocialButton
+          className="ml-4"
+          href="https://google.com"
+          type="instagram"
+        />
       </div>
     </div>
   </div>
