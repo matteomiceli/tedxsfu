e3b300b5c3ffc25eb01553eacf7df71e0c94197a
Matteo Miceli <omatteomiceli@gmail.com>
2021-08-28 19:33:29 -0700

    weird linting error

--- src/components/FullWidthContainer.jsx
+++ src/components/FullWidthContainer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function FullWidthContainer({children}) {
   return (
     <div className="h-screen w-full text-white">
-      <div className="relative top-1/4 w-5/6 mx-auto">
+      <div className="relative top-1/4 w-5/6 mx-auto h-full flex">
         {children}
       </div>
     </div>
--- src/pages/team/teamBios.jsx
+++ src/pages/team/teamBios.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
-import FullWidthContainer from '../../components/FullWidthContainer';
+import React from "react";
+import FullWidthContainer from "../../components/FullWidthContainer";
 
 function TeamBios({ location }) {
   const team = location.state.team;
 
-  return(
+  return (
     <FullWidthContainer>
-      {team.members.map((member) => {
-        
+      {team.members.map((member, i) => {
+        return <BioContainer member={member} i={i} />;
       })}
     </FullWidthContainer>
   );
 }
 
 export default TeamBios;
+
+
+function BioContainer({ member, i }) {
+  return (
+    <div className="text-white">
+      test
+    </div>
+  );
+};
