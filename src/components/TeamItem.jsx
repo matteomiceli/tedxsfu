556122cf5e7826778de14f960a8ec7a2b4d7eeca
Alvin Leung <alvinleung2009@gmail.com>
2021-08-31 11:35:51 -0700

    Page generation for meet the team page

--- src/components/TeamItem.jsx
+++ src/components/TeamItem.jsx
@@ -1,18 +1,20 @@
 import React, { useEffect } from "react";
 import { Link } from "gatsby";
-import Overlay from "./Overlay";
-import loadable from "@loadable/component";
-// import { StaticImage } from "gatsby-plugin-image"
 
 function TeamItem({ team, width, delta, scroll }) {
   return (
     <div id={`team-${team.i}`} className="team-scroll bg-black h-full">
       <div className="flex h-full w-full">
         <div className="relative top-1/4 mt-2">
-          <Link to={`/team/teamBios`} state={{ team: team }} >
-            <img className="team-image" src={team.img} alt={team.team} />
+          <Link to={`/team/${team.href}`}>
+            <img
+              className="team-image"
+              src={team.img}
+              alt={team.team}
+              loading="lazy"
+            />
           </Link>
-          <Link className="flex mt-4" to={`/team/teamBios`} state={{ team: team }} >
+          <Link className="flex mt-4" to={`/team/${team.href}`}>
             <h2 className="font-semibold mr-36">{team.team}</h2>
             {team.members.map((member, i) => {
               return <TeamMembers member={member} />;
--- src/pages/team/teamBios.jsx
+++ src/pages/team/teamBios.jsx
@@ -5,56 +5,54 @@ import { StaticImage } from "gatsby-plugin-image";
 import arrow from "../../static/images/upArrow.svg";
 import linkedIn from "../../static/images/icons/linkedIn.svg";
 import instagram from "../../static/images/icons/instagram.svg";
-import facebook from "../../static/images/icons/facebook.svg"
+import facebook from "../../static/images/icons/facebook.svg";
 
-function TeamBios({ location, history }) {
-  
-  if (location.state) {
+function TeamBios({ location, history, pageContext }) {
+  // Entries in the team.js object
+  const { teamInfo } = pageContext;
 
-    const team =  location.state.team;
-
-    return (
-      <HorizontalScrollContainer>
-        <div className="h-3/5 flex relative top-1/4">
-          <div className="w-full mr-36 pl-24 h-full flex flex-col justify-center">
-            <h1 className="text-5xl w-96 mb-8">{team.team}</h1>
-            <p className="mb-8">{team.teamBio}</p>
-            <Button
-              className="cursor-pointer w-24"
-              href='/team'
-              secondary
-            >
-              Back
-            </Button>
-          </div>
-  
-          <div className="flex h-full">
-            {team.members.map((member, i) => {
-              return <BioContainer member={member} i={i} key={i} />;
-            })}
-          </div>
+  return (
+    <HorizontalScrollContainer>
+      <div className="h-3/5 flex relative top-1/4">
+        <div className="w-full mr-36 pl-24 h-full flex flex-col justify-center">
+          <h1 className="text-5xl w-96 mb-8">{teamInfo.team}</h1>
+          <p className="mb-8">{teamInfo.teamBio}</p>
+          <Button className="cursor-pointer w-24" href="/team" secondary>
+            Back
+          </Button>
         </div>
-      </HorizontalScrollContainer>
-    );
-  } else {
-    return (
-      <p>team not selected</p>
-    );
-  }
-  }
-  
+
+        <div className="flex h-full">
+          {teamInfo.members.map((member, i) => {
+            return <BioContainer member={member} i={i} key={i} />;
+          })}
+        </div>
+      </div>
+    </HorizontalScrollContainer>
+  );
+}
 
 export default TeamBios;
 
 function BioContainer({ member, i }) {
   return (
-    <div className={member.imgWide ? "bio-container-wide text-white flex flex-col mr-36" : "bio-container text-white flex mr-36"}>
+    <div
+      className={
+        member.imgWide
+          ? "bio-container-wide text-white flex flex-col mr-36"
+          : "bio-container text-white flex mr-36"
+      }
+    >
       <img
         className={`teambio-img pr-8 self-${i % 2 === 0 ? "start" : "end"}`}
         src={member.img}
         alt=""
       />
-      <div className={`flex flex-col justify-${i % 2 === 0 ? "start" : "end"} ${member.imgWide ? 'mt-6' : ''}`}>
+      <div
+        className={`flex flex-col justify-${i % 2 === 0 ? "start" : "end"} ${
+          member.imgWide ? "mt-6" : ""
+        }`}
+      >
         <h3 className="text-lg">{member.name}</h3>
         <p className="text-gray-400 mb-4 text-sm">{member.role}</p>
         <p className="mb-6 text-sm">{member.bio}</p>
