bd74d48835e403077a408c0a73dbf646669072ae
Matteo Miceli <omatteomiceli@gmail.com>
2021-08-30 17:48:12 -0700

    working on mobile nav speaker

--- src/components/SpeakerMobileNav.jsx
+++ src/components/SpeakerMobileNav.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 
+import speakers from "../content/speakers";
+
 function SpeakerMobileNav() {
+  const windowWidth = window.innerWidth / 2;
+  
   return (
     <div className="absolute text-white text-4xl w-full bottom-20">
-      <div className="h-24 bg-ted-red flex">
-        <div className="bg-green-400 w-1/2"></div>
+      <div className="h-24 bg-ted-red flex overflow-x-scroll">
+        <div className="mobile-panel-spacer bg-green-400 w-96"></div>
+        {speakers.map(() => {
+          return <SpeakerMobilePanel />
+        })}
       </div>
     </div>
   );
 }
 
 export default SpeakerMobileNav;
+
+function SpeakerMobilePanel() {
+  return <div className="speaker-mobile-panel h-full bg-blue-400 z-10"></div>;
+}
