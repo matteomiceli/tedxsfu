a9449af9704704913cee5652c00293965c3bcfc4
Alvin Leung <alvinleung2009@gmail.com>
2021-09-03 15:13:59 -0700

    Implement vertical sizing using -webkit-fill-available and 100% over 100vh

--- src/components/HorizontalScrollContainer.jsx
+++ src/components/HorizontalScrollContainer.jsx
@@ -1,44 +1,49 @@
 import React, { useEffect, useState, useRef } from "react";
 import loadable from "@loadable/component";
 
-const HorizontalScrollContainer = React.forwardRef(({ children }, ref) => {
-  // page width
-  const [width, setWidth] = useState(0);
-  // page scroll location
-  const [scroll, setScroll] = useState(0);
+const HorizontalScrollContainer = React.forwardRef(
+  ({ children, className }, ref) => {
+    // page width
+    const [width, setWidth] = useState(0);
+    // page scroll location
+    const [scroll, setScroll] = useState(0);
 
-  // mouse scroll delta value
-  const [deltaVal, setDelta] = useState(0);
-  const scrollRef = useRef();
+    // mouse scroll delta value
+    const [deltaVal, setDelta] = useState(0);
+    const scrollRef = useRef();
 
-  const handleScroll = (e) => {
-    // e.preventDefault();
-    setDelta(e.deltaY);
-    setScroll(e.currentTarget.scrollLeft);
+    const handleScroll = (e) => {
+      // e.preventDefault();
+      setDelta(e.deltaY);
+      setScroll(e.currentTarget.scrollLeft);
 
-    // set width of page
-    setWidth(e.currentTarget.scrollWidth - e.currentTarget.clientWidth);
-  };
-
-  // set up global scroll listener
-  useEffect(() => {
-    window.addEventListener("wheel", handleScroll);
-    return () => {
-      window.removeEventListener("wheel", handleScroll);
+      // set width of page
+      setWidth(e.currentTarget.scrollWidth - e.currentTarget.clientWidth);
     };
-  }, []);
 
-  useEffect(() => {
-    scrollRef.current.scrollLeft += deltaVal;
-    setDelta(0);
-  }, [deltaVal]);
+    // set up global scroll listener
+    useEffect(() => {
+      window.addEventListener("wheel", handleScroll);
+      return () => {
+        window.removeEventListener("wheel", handleScroll);
+      };
+    }, []);
 
-  return (
-    <div ref={mergeRefs(scrollRef, ref)} className="outer-scroll-container">
-      <div className="inner-scroll-container">{children}</div>
-    </div>
-  );
-});
+    useEffect(() => {
+      scrollRef.current.scrollLeft += deltaVal;
+      setDelta(0);
+    }, [deltaVal]);
+
+    return (
+      <div
+        ref={mergeRefs(scrollRef, ref)}
+        className={`outer-scroll-container h-full ${className}`}
+      >
+        <div className="inner-scroll-container h-full">{children}</div>
+      </div>
+    );
+  }
+);
 
 //https://www.davedrinks.coffee/how-do-i-use-two-react-refs/
 const mergeRefs = (...refs) => {
